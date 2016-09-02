d509e78cc83034eb620851b78807f6c06b76f3ff
Sylvia Harrington <sylvia.harrington28@gmail.com>
2016-09-02 17:54:57 +0100

    arm64: MMU functions.
    Signed-off-by: Henry Harrington <henry.harrington@gmail.com>

--- source/arch/arm64/include/arch/page.h
+++ source/arch/arm64/include/arch/page.h
@@ -26,5 +26,6 @@
 /** Page size definitions. */
 #define PAGE_WIDTH          12              /**< Width of a page in bits. */
 #define PAGE_SIZE           0x1000          /**< Size of a page. */
+#define LARGE_PAGE_SIZE     0x200000        /**< Size of a large page. */
 
 #endif /* __ARCH_PAGE_H */
